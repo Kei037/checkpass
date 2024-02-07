16ed90985b5ef4fcf6ee30d1b02a05c3af152179
junyoung <juneway@hanmail.net>
2024-02-07 13:44:22 +0900

    chackpass base commit

--- src/main/java/com/checkpass/controller/AdminController.java
+++ src/main/java/com/checkpass/controller/AdminController.java
@@ -1,14 +1,24 @@
 package com.checkpass.controller;
 
+import com.checkpass.dto.AdminDTO;
+import com.checkpass.service.AdminService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @Log4j2
 @RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminController {
+    private final AdminService adminService;
+
     @GetMapping("/login")
     public void adminLoginPage() {
         log.info("adminLoginPage...");
@@ -17,5 +27,14 @@ public class AdminController {
     @GetMapping("/join")
     public void adminJoinPage() {
         log.info("joinPage...");
+    }
+
+    @PostMapping("/join")
+    public String joinPost(@Valid AdminDTO adminDTO, BindingResult bindingresult,
+                         RedirectAttributes redirectAttributes) {
+        log.info("joinPost..!!");
+        Long ano = adminService.register(adminDTO);
+        redirectAttributes.addFlashAttribute("result", ano);
+        return "redirect:/admin/login";
     }
 }
